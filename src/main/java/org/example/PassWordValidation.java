c5422b1a6c87755cc254d9e07bbd7b877069c6ba
murhaf2211 <orfali.murhaf@web.de>
2024-08-21 17:29:02 +0200

    the methods for password validation,at least 8 characters long, contains digits, uppercase and lowercase letters

--- src/main/java/org/example/PassWordValidation.java
+++ src/main/java/org/example/PassWordValidation.java
@@ -1,8 +1,72 @@
 package org.example;
+import java.util.Scanner;
 
 public class PassWordValidation {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String password;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your Password");
+        password = scan.nextLine();
+        passlength(password);
+
+
+
     }
+    public static void passlength(String pass){
+        int min = 8;
+        int max =16;
+        if ( pass.length() >= min && pass.length() < max){
+           int digitCounter=0;
+           int upperCaseCounter=0;
+           int lowerCaseCounter=0;
+           for(int i=0 ; i < pass.length() ; i++){
+               char c =pass.charAt(i);
+               if(Character.isDigit(c)){
+                   digitCounter++;
+               }
+               if(Character.isUpperCase(c)){
+                   upperCaseCounter++;
+               }
+               if(Character.isLowerCase(c)){
+                   lowerCaseCounter++;
+               }
+           }
+               if(digitCounter > 0 && upperCaseCounter >0 && lowerCaseCounter >0){
+                   System.out.println("Password  is strong !!");
+               }else if (digitCounter <= 0){
+                   System.out.println(" password  must have digits");
+                   String password;
+                   Scanner scan = new Scanner(System.in);
+                   System.out.println("Enter your Password");
+                   password = scan.nextLine();
+                   passlength(password);
+               }else if (upperCaseCounter <= 0){
+                   System.out.println(" password  must have UpperCase char");
+                   String password;
+                   Scanner scan = new Scanner(System.in);
+                   System.out.println("Enter your Password");
+                   password = scan.nextLine();
+                   passlength(password);
+               }else if (lowerCaseCounter <= 0){
+                   System.out.println(" password  must have LowerCase char");
+                   String password;
+                   Scanner scan = new Scanner(System.in);
+                   System.out.println("Enter your Password");
+                   password = scan.nextLine();
+                   passlength(password);
+               }
+
+           } else{
+           System.out.println("Enter min 8 char password ");
+           String password;
+           Scanner scan = new Scanner(System.in);
+           System.out.println("Enter your Password");
+           password = scan.nextLine();
+           passlength(password);
+       }
+    }
+
 }
---
+++ src/test/java/org/example/PassWordValidationTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PassWordValidationTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+
+    @Test
+    void testMain() {
+    }
+
+    @Test
+    void passlength() {
+    }
+
+
+}
